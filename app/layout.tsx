7e03a1c9a776dc6dad94a4fa3b41a47e0359fdd3
prasannakarki77 <prasanna@ayatdigital.com>
2023-05-05 19:53:43 +0545

    added country select option

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,12 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 
+// modals
+import RentModal from "./components/modals/RentModal";
 import RegisterModal from "./components/modals/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
+
+import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
@@ -31,6 +34,7 @@ export default async function RootLayout({
           <ToasterProvider />
           <RegisterModal />
           <LoginModal />
+          <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
